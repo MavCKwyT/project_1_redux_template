0016d912cd6132203b6aedf6eedf37fc14a8c3a2
evgenii <evgenklochkov163@gmail.com>
2022-07-20 22:36:56 +0400

    intermediate commit

--- src/context/searchContext/index.ts
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-import { ISearchContext } from 'context/searchContext/types';
-
-const defaultValue = {};
-export const SearchContext = createContext<ISearchContext>(defaultValue);
